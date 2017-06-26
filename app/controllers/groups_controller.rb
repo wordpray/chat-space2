0fdf2eef2ec49bae786ceabe65b46d40d5a9cc15
y.yoshiki <labiatejoker@gmail.com>
2017-06-26 19:59:09 +0900

    Set groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,8 +3,35 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def edit
-    @group = Group.new
+  def index
+    @groups = current_user.groups
   end
-  
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループが作成されました。'
+    else
+      redirect_to :back, alert: 'グループ作成に失敗しました。'
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループ情報が更新されました。'
+    else
+      redirect_to :back, alert: 'グループ情報更新に失敗しました。'
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids:[])
+  end
 end
