de5a3f29d459b980a87d516e0c5fbc25f4b9acdc
y.yoshiki <labiatejoker@gmail.com>
2017-06-26 15:16:04 +0900

    Set routing of groups

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy"
   end
-
+  resources :groups, only:[:create,:new,:edit,:update]
   root 'messages#index'
 end
