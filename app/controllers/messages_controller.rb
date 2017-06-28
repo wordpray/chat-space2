fb9bde0cf1be8377df20161ca80a967225b5761a
y.yoshiki <labiatejoker@gmail.com>
2017-06-28 14:52:55 +0900

    Definition of the controller breadth

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,20 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-    @group = Group.find(1)
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @message = current_user.messages.new(message_params)
+    @message.save
+    redirect_to group_messages_path(@group)
+  end
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:body, :image).merge(params.permit[:group_id])
 end
