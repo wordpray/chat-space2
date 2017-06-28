374f4eecc1cd2572d77b3ce835df8b1235240a8f
y.yoshiki <labiatejoker@gmail.com>
2017-06-28 17:27:35 +0900

    Set models / message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  mount_uploader :image, ImageUploader
+
+  validates :text_or_image, presence: true
+
+  private
+
+  def text_or_image
+    body || image
+  end
+
 end
