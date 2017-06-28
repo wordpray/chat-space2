8cd11c0db337c6b9a82877c67701b744b0686d45
y.yoshiki <labiatejoker@gmail.com>
2017-06-28 17:27:59 +0900

    Set details about messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,30 @@
 class MessagesController < ApplicationController
-  before_action :set_group, only: [:index, :create]
+  before_action :set_projects, only: [:index, :create]
 
   def index
-    @groups = current_user.groups
-    @messages = @group.messages.includes(:user)
+    @message = Message.new
   end
 
   def create
     @message = current_user.messages.new(message_params)
-    @message.save
-    redirect_to group_messages_path(@group)
+    if @message.save
+      redirect_to  group_messages_path(@group), notice: '投稿しました。'
+    else
+      flash.now[:alert] = "メッセージを入力して下さい。"
+      render :index
+    end
   end
 end
 
 private
 
 def message_params
-  params.require(:message).permit(:body, :image).merge(params.permit[:group_id])
+  params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
 end
 
-def set_group
+def set_projects
   @group = Group.find(params[:group_id])
+  @groups = current_user.groups
+  @users = @group.users
+  @messages = @group.messages.includes(:user)
 end
