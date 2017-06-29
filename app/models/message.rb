d97ad78cf14f1aea7e0fcf72b7988c58c8099da5
y.yoshiki <labiatejoker@gmail.com>
2017-06-29 17:37:36 +0900

    Message model validation settings

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,11 @@ class Message < ApplicationRecord
   belongs_to :group
   mount_uploader :image, ImageUploader
 
-  validates :text_or_image, presence: true
+  validates :body_or_image, presence: true
+  validates :group_id, presence: true
+  validates :user_id, presence: true
 
-  def text_or_image
+  def body_or_image
     body || image
   end
 end
