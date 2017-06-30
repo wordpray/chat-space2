da8b753824ba623b766b8d114d417bb1404bc378
y.yoshiki <labiatejoker@gmail.com>
2017-06-30 19:44:39 +0900

    Modification of description and addition of test code

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -19,20 +19,41 @@ describe MessagesController, type: :controller do
     context 'When log in' do
       before do
         login_user user
+        get :index, params: { group_id: group.id }
       end
       it "assigns the requested tweet to @message" do
-        get :index, params: { group_id: group.id }
         expect(assigns(:message)).to be_a_new(Message)
       end
+
+      it "assigns the requested tweet to @messages" do
+        messages = group.messages
+        expect(assigns(:messages)).to match(messages.sort{|a,b| b.created_at <=> a.created_at})
+      end
+
+      it "assigns the requested tweet to @group" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "assigns the requested tweet to @groups" do
+        groups = user.groups
+        expect(assigns(:groups)).to eq groups
+      end
+
+      it "assigns the requested tweet to @users" do
+        users = group.users
+        expect(assigns(:users)).to eq users
+      end
+
       it "renders the :index template" do
-        get :index, params: { group_id: group.id }
         expect(response).to render_template :index
       end
     end
     
     context 'When log out' do
-      it "is redirected to the intended view" do
+      before do
         get :index, params: { group_id: group.id }
+      end
+      it "is redirected to the intended view" do
         expect(response).to redirect_to new_user_session_path
       end
     end
@@ -46,6 +67,7 @@ describe MessagesController, type: :controller do
       it "saves the message" do
         expect {post :create, params: message_params}.to change(Message, :count).by(1)
       end
+
       it "is redirected to the intended view" do
         post :create, params: message_params
         expect(response).to redirect_to group_messages_path(group.id)
@@ -59,6 +81,7 @@ describe MessagesController, type: :controller do
       it "is not saved" do
         expect{post :create, params: message_empty}.to_not change(Message, :count)
       end
+
       it "is redirected to the intended view" do
         post :create, params: message_empty
         expect(response).to render_template :index
