aa0ac0d1b1ef2589c2130b993f5dd0cefa620214
y.yoshiki <labiatejoker@gmail.com>
2017-07-03 19:07:26 +0900

    Correcting comment part

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,7 +12,7 @@ $(function(){
   function body_only(message){
     var html = `
                 <p class="chat-main__message-name"> ${message.user_name} </p>
-                <p class="chat-main__message-time"> ${message.created_at.strftime("%Y/%m/%d %H:%M:%S")} </p>
+                <p class="chat-main__message-time"> ${message.created_at} </p>
                 <p class="chat-main__message-body"> ${message.body} </p>
                 `;
     return html;
@@ -33,11 +33,11 @@ $(function(){
     })
     .done(function(data){
       if(data.image.url === null){
-        var html = text(data);
+        var html = body_only(data);
         $('.chat-main__body--messages-list').prepend(html);
         $('.chat-main__form').val('');
       }else{
-        var html = text_and_image(data);
+        var html = body_and_image(data);
         $('.chat-main__body--messages-list').prepend(html);
         $('.chat-main__form').val('');
       }
