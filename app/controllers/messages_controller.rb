0105a542c7886007647819eb467a438b012d2d3f
y.yoshiki <labiatejoker@gmail.com>
2017-07-03 18:22:13 +0900

    Change messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,10 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
-      redirect_to  group_messages_path(@group), notice: '投稿しました。'
+      respond_to do |format|
+        format.json
+        format.html {redirect_to  group_messages_path(@group), notice: '投稿しました。'}
+      end
     else
       flash.now[:alert] = "メッセージを入力して下さい。"
       render :index
